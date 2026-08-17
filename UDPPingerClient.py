c4188630162f7e4e3f034bbea2d48d9b1d222b32
Felix Karlsson <sout.coding@gmail.com>
2026-08-17 18:08:20 +0200

    Changed from camel case to python underscore convention

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -1,48 +1,49 @@
 import socket
 import time
 
-serverName = "192.168.1.183"
-serverPort = 13000
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-clientSocket.settimeout(1.0)
+SERVER_NAME = "192.168.1.183"
+SERVER_PORT = 13000
+CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+CLIENT_SOCKET.settimeout(1.0)
+NUM_PACKETS = 10
 
-rttArray = []
-packetsSent = 0
+rtt_array = []
+packets_sent = 0
 
-for sequence in range(1, 11):
-    packetsSent += 1
+for sequence in range(1, NUM_PACKETS + 1):
+    packets_sent += 1
     try:
         message = f"Packet {sequence} | timestamp {time.time():.2f}"
-        startTime = time.perf_counter()
-        clientSocket.sendto(message.encode(), (serverName, serverPort))
-        receivedMessage, serverAddress = clientSocket.recvfrom(1024)
-        endTime = time.perf_counter()
-        rtt = (endTime - startTime) * 1000
-        print(f"Message from {serverAddress}: {receivedMessage.decode()} |  RTT: {rtt:.3f}ms")
-        rttArray.append(rtt)
+        start_time = time.perf_counter()
+        CLIENT_SOCKET.sendto(message.encode(), (SERVER_NAME, SERVER_PORT))
+        received_message, server_address = CLIENT_SOCKET.recvfrom(1024)
+        end_time = time.perf_counter()
+        rtt = (end_time - start_time) * 1000
+        print(f"Message from {server_address}: {received_message.decode()} |  RTT: {rtt:.3f}ms")
+        rtt_array.append(rtt)
     except socket.timeout:
         print(f"Packet {sequence} timed out")
         continue
 
-packetsReceived = len(rttArray)
+packets_received = len(rtt_array)
 
 
-if packetsReceived > 0:
-    minRtt = min(rttArray)
-    maxRtt = max(rttArray)
-    rttSum = 0
+if packets_received > 0:
+    min_rtt = min(rtt_array)
+    max_rtt = max(rtt_array)
+    rtt_sum = 0
 
-    for i in range(packetsReceived):
-        rttSum += rttArray[i]
+    for i in range(packets_received):
+        rtt_sum += rtt_array[i]
 
-    avgRtt = rttSum / packetsReceived
+    avg_rtt = rtt_sum / packets_received
 
-    lostPacketsPercentage = (packetsSent - packetsReceived) / packetsSent* 100
+    lost_packets_percentage = (packets_sent - packets_received) / packets_sent * 100
     print("---Statistics---")
-    print(f"{packetsSent} packets sent, {packetsReceived} received, {lostPacketsPercentage:.2f}% packet loss")
-    print(f"Min RTT: {minRtt:.3f}ms | Max RTT: {maxRtt:.3f}ms | Avg RTT: {avgRtt:.3f}ms")
+    print(f"{packets_sent} packets sent, {packets_received} received, {lost_packets_percentage:.2f}% packet loss")
+    print(f"Min RTT: {min_rtt:.3f}ms | Max RTT: {max_rtt:.3f}ms | Avg RTT: {avg_rtt:.3f}ms")
 
 
-clientSocket.close()
+CLIENT_SOCKET.close()
 
 
